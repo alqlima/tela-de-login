b923d2fe056970107c1d187bee471a10a9eb3f59
André Queiroz <alqlima@gmail.com>
2020-08-31 09:48:27 -0300

    Add events

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
+
 console.log('Olá Mundo!')
+
+document.getElementById('btn-submit').addEventListener('click', e => {
+	console.log('O botão foi clicado')
+});
+document.getElementById('form-login').addEventListener('mouseenter', e => {
+	console.log('O mouse está sobre o formuário')
+});
+document.querySelector('#form-login').addEventListener('mouseleave', e => {
+	console.log('O mouse está fora do formulário')
+});
+document.querySelector('#form-login').addEventListener('submit', e => {
+	e.preventDefault();
+	console.log('O formuário foi enviado! Aqui vai o ajax...')
+});
