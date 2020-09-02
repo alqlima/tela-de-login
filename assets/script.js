79ab6b32f292cd739de458a687273d4327e91e8a
André Queiroz <alqlima@gmail.com>
2020-09-02 11:28:02 -0300

    Update script.js
    Working with JSON

--- assets/script.js
+++ assets/script.js
@@ -11,9 +11,29 @@ document.querySelector('#form-login').addEventListener('mouseleave', e => {
 	console.log('O mouse está fora do formulário')
 });
 document.querySelector('#form-login').addEventListener('submit', e => {
+	
 	e.preventDefault();
+	
 	let email = document.querySelector('#email').value;
+	
 	let password = document.querySelector('#password').value;
-	console.log(email, password)
+	
+	//console.log(email, password);
+	
+	let json = {
+		email,
+		password
+	}
+	
+	console.log(json);
+	
+	let stringJSON = JSON.stringify(json);
+	
+	console.log(stringJSON);
+	
+	let jsonParse = JSON.parse(stringJSON);
+	
+	console.log(jsonParse);
+	
 	
 });
